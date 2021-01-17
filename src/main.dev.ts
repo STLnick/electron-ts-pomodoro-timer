55548d496e6b7d30340b86ff6e62f602739d23c2
Nick Ray <nickrray@gmail.com>
2021-01-17 00:06:42 -0600

    :construction: :heavy_plus_sign: Add Express and perform basic setup

--- src/main.dev.ts
+++ src/main.dev.ts
@@ -14,6 +14,7 @@ import path from 'path';
 import { app, BrowserWindow, shell } from 'electron';
 import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
+import express from 'express';
 import MenuBuilder from './menu';
 
 export default class AppUpdater {
@@ -23,6 +24,14 @@ export default class AppUpdater {
     autoUpdater.checkForUpdatesAndNotify();
   }
 }
+
+// Example starter code for Express backend routing from ERB github
+const expressApp = express();
+expressApp.get('/', (_, res) => res.send('Hello World!'));
+expressApp.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
+/* - - - - - - - - - - - - - */
 
 let mainWindow: BrowserWindow | null = null;
 
